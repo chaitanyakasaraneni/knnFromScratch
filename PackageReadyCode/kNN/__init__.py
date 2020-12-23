c9883469434c0a7acc1de5473e26ff660593b454
chaitanyakasaraneni <chaitanyakrishna755@gmail.com>
2020-12-23 15:31:11 -0800

    del: unwanted code

--- PackageReadyCode/kNN/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-import numpy as np
-import pandas as pd
-import operator
-from .distanceMetrics import distanceMetrics
-from .kNNClassifier import kNNClassifier
-from .kFoldCV import kFoldCV
